59c3ad9384300159e7d1b52c75c4dc2c69db3f7f
Prakash Rana <prana12@live.com>
2022-07-24 19:45:06 +0100

    createClient mutation

--- src/main/java/dev/lab/demo/controller/ClientController.java
+++ src/main/java/dev/lab/demo/controller/ClientController.java
@@ -7,6 +7,7 @@ import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -25,6 +26,20 @@ public class ClientController {
     @QueryMapping
     public Client findClient(@Argument String emailAddress) {
         return clientRepository.findClientByEmail(emailAddress);
+    }
+
+    @MutationMapping
+    public Client createClient(@Argument String firstName, @Argument String lastName, @Argument String emailAddress, @Argument String dateOfBirth, @Argument String addressLine1, @Argument String postcode) {
+        Client client = new Client();
+        client.setFirstName(firstName);
+        client.setLastName(lastName);
+        client.setEmailAddress(emailAddress);
+        client.setDateOfBirth(LocalDate.parse(dateOfBirth));
+        client.setAddressLine1(addressLine1);
+        client.setPostcode(postcode);
+        clientRepository.save(client);
+
+        return client;
     }
 
     @MutationMapping
